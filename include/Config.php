b95cfa927e70d2fab8ffb2f3ef9f535d3adeb7d1
shgini27 <go_shuhrat@yahoo.com>
2017-08-31 09:04:16 +0500

    08-31-2017

---
+++ include/Config.php
@@ -0,0 +1,16 @@
+<?php
+#Config.php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "sb19861905");
+define("DB_DATABASE", "android_api");
